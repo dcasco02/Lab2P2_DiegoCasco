561ac36afa91feb1049f201d86fda525265c16f0
dcasco02 <dcasco@unitec.edu>
2022-04-29 14:08:30 -0600

    Cambios 2

--- src/lab2p2_diegocasco/Lab2P2_DiegoCasco.java
+++ src/lab2p2_diegocasco/Lab2P2_DiegoCasco.java
@@ -6,6 +6,7 @@
 package lab2p2_diegocasco;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -16,20 +17,21 @@ import javax.swing.JOptionPane;
 public class Lab2P2_DiegoCasco {
 
     static Scanner lea = new Scanner(System.in);
-
+    static Random r = new Random();
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         ArrayList<String> jugadores = new ArrayList();
         ArrayList<String> Jugadas = new ArrayList();
+        ArrayList<String> numrandom = new ArrayList();
         int opcion = 60;
         while (opcion != 0) {
             opcion = Integer.parseInt(
                     JOptionPane.showInputDialog(
                             "0-Salida\n"
                             + "1-Crear Jugadores\n"
-                            + "2-Listar Jugardores\n"
+                            + "2-Listar Jugardores por posicion\n"
                             + "3-Listar Todo\n"
                             + "4-Jugar"));
             if (opcion == 1) {
@@ -38,12 +40,15 @@ public class Lab2P2_DiegoCasco {
                 jugadores.add(n);
             }
             if (opcion == 2) {
-                for (String j : jugadores) {
-                    System.out.println(j);
-                }
+                System.out.println("Ingrese la posicion que desea ver: ");
+                int p=lea.nextInt();
+                System.out.println(jugadores.get(p));
             }if(opcion==3){
                 
             }if(opcion==4){
+                for(int i=0;i<=4;i++){
+                    
+                }
                 
             }
         }
