7264a740d1d99bb188b5d1095c9f8f1b4fd93c2d
dcasco02 <dcasco@unitec.edu>
2022-04-29 13:47:57 -0600

    menu

--- src/lab2p2_diegocasco/Lab2P2_DiegoCasco.java
+++ src/lab2p2_diegocasco/Lab2P2_DiegoCasco.java
@@ -5,17 +5,43 @@
  */
 package lab2p2_diegocasco;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author dcasc
  */
 public class Lab2P2_DiegoCasco {
 
+    static Scanner lea = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<String> jugadores = new ArrayList();
+        int opcion = 60;
+        while (opcion != 0) {
+            opcion = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            "0-Salida\n"
+                            + "1-Crear Jugadores\n"
+                            + "2-Listar Jugardores\n"
+                            + "3-Listar Todo\n"
+                            + "4-Jugar"));
+            if (opcion == 1) {
+                System.out.println("Ingrese el nombre del jugador: ");
+                String n = lea.nextLine();
+                jugadores.add(n);
+            }
+            if (opcion == 2) {
+                for (String j : jugadores) {
+                    System.out.println(j);
+                }
+            }
+        }
     }
     
 }
