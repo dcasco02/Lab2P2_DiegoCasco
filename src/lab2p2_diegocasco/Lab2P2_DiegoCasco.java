85782a2ee600547104f29a933b023cff631713e9
dcasco02 <dcasco@unitec.edu>
2022-04-29 15:59:32 -0600

    Cambios

--- src/lab2p2_diegocasco/Lab2P2_DiegoCasco.java
+++ src/lab2p2_diegocasco/Lab2P2_DiegoCasco.java
@@ -18,11 +18,12 @@ public class Lab2P2_DiegoCasco {
 
     static Scanner lea = new Scanner(System.in);
     static Random r = new Random();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Personas per =new Personas();
+        Personas per = new Personas();
         ArrayList jugadores = new ArrayList();
         ArrayList<String> numrandom = new ArrayList();
         int opcion = 60;
@@ -37,35 +38,41 @@ public class Lab2P2_DiegoCasco {
             if (opcion == 1) {
                 System.out.println("Ingrese el nombre del jugador: ");
                 String n = lea.nextLine();
-                jugadores.add(new Personas(n,0));
+                jugadores.add(new Personas(n, 0));
             }
             if (opcion == 2) {
                 if (jugadores.size() == 0) {
                     System.out.println("No hay Jugadores");
-                }else{
+                } else {
                     System.out.println("Ingrese la posicion que desea ver: ");
-                    int p = lea.nextInt(); 
+                    int p = lea.nextInt();
                     System.out.println(jugadores.get(p));
                 }
-            }if(opcion==3){
+            }
+            if (opcion == 3) {
                 imprimirrecursivo(jugadores, opcion);
-            }if(opcion==4){
-                int turnos=0;
-                for(int i=0;i<4;i++){
-                    numrandom.add(String.valueOf(0+r.nextInt(7))); 
+            }
+            if (opcion == 4) {
+                int turnos = 0;
+                for (int i = 0; i < 4; i++) {
+                    numrandom.add(String.valueOf(0 + r.nextInt(7)));
                 }
-                while(turnos!=8){
-                    
+                while (turnos != 8) {
+                    for (Object j : jugadores ) {
+                        
+                    }
                 }
             }
         }
     }
-      public static void imprimirrecursivo (ArrayList jugadores, int i) {
-        if (i == jugadores.size()-1) { System.out.println (jugadores.get(i)); }
-        else {
+
+    public static void imprimirrecursivo(ArrayList jugadores, int i) {
+        if (i == jugadores.size() - 1) {
             System.out.println(jugadores.get(i));
-            imprimirrecursivo(jugadores,i+1);
-        }       
+        } else {
+            System.out.println(jugadores.get(i));
+            imprimirrecursivo(jugadores, i + 1);
+        }
     }
-    
+
 }
